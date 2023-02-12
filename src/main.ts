9b5bcf35461406ffff6f552e3c9b24708b46f494
Rogger Ortiz <rogger.ortiz.br@gmail.com>
2023-02-12 07:21:04 -0500

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('Products')
+    .setDescription('Products API')
+    .setVersion('1.0')
+    .addTag('Products')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
